73b7a87041bca09462cdb4d14b3096b0b0933eef
nuzzi123 <nuzzi123@gmail.com>
2018-11-07 16:30:25 +0200

    trying

--- dataAccess/connection.js
+++ dataAccess/connection.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('mysql://sql7263189:8QXBVBnXgL@sql7.freesqldatabase.com/sql7263189');
+const sequelize = new Sequelize(process.env.CONNECTION || 'mysql://sql7263189:8QXBVBnXgL@sql7.freesqldatabase.com/sql7263189');
 
 sequelize
     .authenticate()
