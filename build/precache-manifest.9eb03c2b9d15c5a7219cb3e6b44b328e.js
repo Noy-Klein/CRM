7616897688f5932b4b5d2e640e7c0805f88d25fe
nuzzi123 <nuzzi123@gmail.com>
2018-11-07 16:48:48 +0200

    push build

---
+++ build/precache-manifest.9eb03c2b9d15c5a7219cb3e6b44b328e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ddac391b5519ce419c12",
+    "url": "/static/js/main.ddac391b.chunk.js"
+  },
+  {
+    "revision": "792c096d909935cce458",
+    "url": "/static/js/1.792c096d.chunk.js"
+  },
+  {
+    "revision": "ddac391b5519ce419c12",
+    "url": "/static/css/main.02939e18.chunk.css"
+  },
+  {
+    "revision": "f095bbb4ce9082d11a435a929c859b54",
+    "url": "/index.html"
+  }
+];
